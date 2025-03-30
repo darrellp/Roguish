342ca264e998620ccda0cc6e00ee7b7196662a2a
Darrell Plank <jar_czar@msn.com>
2025-03-30 14:46:57 -0700

    Bit more reorg

--- Roguish/GameSettings.cs
+++ Roguish/GameSettings.cs
@@ -7,16 +7,15 @@ internal static class GameSettings
     public const int GameWidth = 130;
     public const int GameHeight = 91;
 
-
     // Size of the dungeon
     public const int DungeonWidth = 90;
     public const int DungeonHeight = GameHeight - 1;
 
     // Starting size of the window into the dungeon
-    public static int DungeonViewWidth { get; } = DungeonWidth;
-    public static int DungeonViewHeight { get; } = DungeonHeight;
+    public const int DungeonViewWidth = DungeonWidth;
+    public const int DungeonViewHeight = DungeonHeight;
 
-    public static int IbWidth = GameWidth - DungeonViewWidth;
+    public const int IbWidth = GameWidth - DungeonViewWidth;
     public const int IbHeight = GameHeight - 1;
     public static Point IbPosition { get; } = Point.Zero;
 
@@ -25,20 +24,19 @@ internal static class GameSettings
     public const int BorderWidthY = 7;
 
     // Status bar size
-    public static int SbWidth { get; } = GameWidth;
+    public const int SbWidth = GameWidth;
     public const int SbHeight = 1;
     public static Point SbPosition { get; } = new(0, GameHeight - 1);
 
     #endregion
 
     #region Misc Settings
-    public static bool FAllowResize => true;
-    public static Settings.WindowResizeOptions ResizeMode => Settings.WindowResizeOptions.None;
+    public const Settings.WindowResizeOptions ResizeMode = Settings.WindowResizeOptions.None;
+    public const int FovRadius = 10;
+    public const int MonstersPerLevel = 20;
+    public static bool FAllowResize => false;
     public static Color ClearColor = Color.Black;
     public static Color FloorColor = Color.Orange;
     public static Color WallColor = Color.Blue;
-    public const int FovRadius = 10;
-    public const int MonstersPerLevel = 20;
-
     #endregion
 }
