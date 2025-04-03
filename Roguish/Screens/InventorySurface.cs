ded354100fca172a71ec8ef9b9bd8e5d32d5adcb
Darrell Plank <jar_czar@msn.com>
2025-04-03 11:46:03 -0700

    Added InventorySurface

---
+++ Roguish/Screens/InventorySurface.cs
@@ -0,0 +1,8 @@
+﻿namespace Roguish.Screens;
+internal class InventorySurface : ScreenSurface
+{
+    public InventorySurface() : base(GameSettings.InvWidth, GameSettings.InvHeight)
+    {
+        Position = GameSettings.InvPosition;
+    }
+}
