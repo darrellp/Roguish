4fa56dd11711b908b631db6246ea3052320fa620
Darrell Plank <jar_czar@msn.com>
2025-04-03 18:45:50 -0700

    Highlighting in inventory screen

--- Roguish/Screens/InventorySurface.cs
+++ Roguish/Screens/InventorySurface.cs
@@ -1,11 +1,13 @@
 ﻿using EcsRx.Extensions;
 using Roguish.ECS.Components;
+using SadConsole.Input;
 using IObservableGroup = EcsRx.Groups.Observable.IObservableGroup;
 
 namespace Roguish.Screens;
 internal class InventorySurface : ScreenSurface
 {
     private static List<InventoryItem> _inventorySlots = new List<InventoryItem>(GameSettings.InvHeight);
+    private static int _selectedIndex = -1;
 
     public InventorySurface() : base(GameSettings.InvWidth, GameSettings.InvHeight)
     {
@@ -20,6 +22,31 @@ internal class InventorySurface : ScreenSurface
             : "Unnamed Object";
         Surface.Print(0, _inventorySlots.Count, name);
         _inventorySlots.Add(new InventoryItem(id, name));
+    }
+
+    protected override void OnMouseLeftClicked(MouseScreenObjectState state)
+    {
+        var (x, y) = state.CellPosition;
+        var index = y;
+        if (index >= _inventorySlots.Count)
+            return;
+        MoveHighlightTo(index);
+    }
+
+    private void MoveHighlightTo(int index)
+    {
+        if (index == _selectedIndex)
+        {
+            return;
+        }
+
+        if (_selectedIndex >= 0)
+        {
+            var name = _inventorySlots[_selectedIndex].name;
+            Surface.Print(0, _selectedIndex, name, Color.White);
+        }
+        _selectedIndex = index;
+        Surface.Print(0, index, _inventorySlots[index].name, Color.Orange);
     }
 
     private record InventoryItem(int id, string name);
